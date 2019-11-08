8a7e6d27b687c4b346725a598dcbd0e9e778a9a8
liqicong <952267787@qq.com>
2019-11-08 17:24:51 +0800

    lruCache写到一半

--- src/main/java/com/lqc/xiaohui/applicationofalgorithm/MyBitMap.java
+++ src/main/java/com/lqc/xiaohui/applicationofalgorithm/MyBitMap.java
@@ -17,6 +17,22 @@ public class MyBitMap {
      */
     private int size;
 
+    public int size() {
+        return this.size;
+    }
+
+    public long[] getWords() {
+        return this.words;
+    }
+
+    public void setWords(long[] words) {
+        this.words = words;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
     public MyBitMap(int size) {
         this.size = size;
         this.words = new long[getWordIndex(size - 1) + 1];
@@ -30,18 +46,42 @@ public class MyBitMap {
         return (words[wordIndex] & (1L << bitIndex)) != 0;
     }
 
-    public void setBit(int bitIndex){
+    public void setBit(int bitIndex) {
         if (bitIndex < 0 || bitIndex > size - 1) {
             throw new IndexOutOfBoundsException("超过BitMap有效范围");
         }
         //先定位到那个word
-        int wordIndex=getWordIndex(bitIndex);
-        words[wordIndex]|=(1L<<bitIndex);
-
+        int wordIndex = getWordIndex(bitIndex);
+        words[wordIndex] |= (1L << bitIndex);
     }
 
     private int getWordIndex(int index) {
         return index >> 6;
+    }
+
+    /**
+     * 实现两个BitMap的与操作
+     *
+     * @param bitMap1 bitMap1
+     * @param bitMap2 bitMap2
+     * @return 与操作后的bitmap
+     * 可能两个bitMap长度不一样
+     */
+    private MyBitMap and(MyBitMap bitMap1, MyBitMap bitMap2) {
+        if (bitMap1.size() < bitMap2.size()) {
+            return and(bitMap2, bitMap1);
+        }
+        MyBitMap myBitMap = new MyBitMap(bitMap1.size());
+        long[] tempData =new long[bitMap1.size()];
+        for (int i = 0; i < bitMap2.size(); i++) {
+            tempData[i] = (bitMap1.getWords()[i] & bitMap2.getWords()[i]);
+        }
+        //把bitMap1剩余的数组接到tempData
+        for(int i=bitMap2.size();i<bitMap1.size();i++){
+            tempData[i]=bitMap1.getWords()[i];
+        }
+        myBitMap.setWords(tempData);
+        return myBitMap;
     }
 
 }
---
+++ src/main/java/com/lqc/xiaohui/applicationofalgorithm/MyLRUCache.java
@@ -0,0 +1,43 @@
+package com.lqc.xiaohui.applicationofalgorithm;
+
+import java.util.HashMap;
+
+/**
+ * @author liqicong@myhexin.com
+ * @date 2019/11/8 15:36
+ */
+public class MyLRUCache {
+    /**
+     * 定义LRU缓存的最大保存个数
+     */
+    private int limit;
+    private Node head;
+    private Node end;
+    private HashMap<String,Node> hashMap;
+    public MyLRUCache(int limit){
+        this.limit=limit;
+        hashMap=new HashMap<>();
+    }
+
+    class Node{
+        Node(String key,String value){
+            this.key=key;
+            this.value=value;
+        }
+        public Node pre;
+        public Node next;
+        public String key;
+        public String value;
+    }
+
+    public void put(String key,String value){
+        Node node=hashMap.get(key);
+        if(end!=null){
+
+        }
+    }
+
+    private void addNode(Node node){
+
+    }
+}
